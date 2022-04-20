9c1c9a8911e5c03b03ec40552fb31856193d8fc7
fhs47805 <alymalym@web.de>
2022-04-20 10:29:49 +0200

    first commit

---
+++ src/random-name.ts
@@ -0,0 +1,4 @@
+export const randomName = ():string => {
+    const names:Array<string> = ['Dwight Schrute', 'Michael Scott', 'Pam Beesly', 'Jim Halpert']
+    return names[Math.floor(Math.random() * names.length)];
+}
---
+++ tests/index.spec.ts
@@ -0,0 +1,7 @@
+/*
+import { randomName } from ".lib/cjs/random-name.js"
+
+test('randomName function', () => {
+    expect
+})
+*/
